78460c4f6c2df62a95de15cb28ac424de9f1a251
bigman2003 <107520563+bigman2003@users.noreply.github.com>
2022-06-16 22:29:28 +0700

    Add files via upload

---
+++ giohang.js
@@ -0,0 +1,33 @@
+function them(button){
+    var row = button.parentElement.parentElement.cloneNode(true);
+    var btnXoa = row.getElementsByTagName("button")[0];
+    btnXoa.innerText = "Xoá";
+    btnXoa.setAttribute('onclick', 'xoa(this)');
+    document.getElementById("cart").appendChild(row);
+    tinhtong(); 
+}
+function xoa(button){
+    var row = button.parentElement.parentElement;
+    document.getElementById("cart").removeChild(row);
+    tinhtong();
+}
+console.log(xoa(button));
+function tinhtong(){
+    var cart = document.getElementById("cart");
+    var rows = cart.getElementsByTagName("tr");
+    var tong = 0;
+    for (var i = 0; i < rows.length; i++){
+        var price  = rows[i].children[2].innerText;
+        price = parseInt(price);
+        tong += price;
+    }
+    document.getElementById("tong").innerText = tong;
+
+}
+console.log(tinhtong());
+document.getElementById("cart").style.display="none";
+function daott(obj){
+    var row = obj.parentElement.parentElement;
+    var btn = row.getElementsByTagName("button")[0];
+    btn.disabled = ! btn.disabled;
+}
